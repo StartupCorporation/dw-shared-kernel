0bd1e385e2bb954aa911d54e7d07d6151631456b
Kirill Panchenko <pnchkirill@gmail.com>
2025-03-18 20:27:39 +0200

    feat: add event bus (#1)
    * feat: add mixins, events bus
    
    * feat: Added event bus

--- src/dw_shared_kernel/infrastructure/layer.py
+++ src/dw_shared_kernel/infrastructure/layer.py
@@ -1,4 +1,6 @@
 from dw_shared_kernel.infrastructure.bus.command.bus import CommandBus
+from dw_shared_kernel.infrastructure.bus.event.bus import EventBus
+from dw_shared_kernel.infrastructure.bus.event.repository import IntegrationEventRepository
 from dw_shared_kernel.infrastructure.bus.query.bus import QueryBus
 from dw_shared_kernel.infrastructure.di.container import Container
 from dw_shared_kernel.infrastructure.di.layer import Layer
@@ -14,3 +16,6 @@ class SharedKernelInfrastructureLayer(Layer):
     ) -> None:
         container[QueryBus] = QueryBus()
         container[CommandBus] = CommandBus()
+        container[EventBus] = EventBus()
+
+        container[IntegrationEventRepository] = IntegrationEventRepository()
